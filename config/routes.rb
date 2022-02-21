60c2e488e44cb9eed86afe6d535c2eca48211fe4
Tenzin Rose <tenzinrose@gmail.com>
2022-02-21 21:20:53 +0000

    User artists page added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :artworks
+  get 'artists/:id', to: 'users#artists'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
