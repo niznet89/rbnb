8a9b73d9a78a2dcfe5f97e0cfe46a79ac0bd47fc
Tempestt Woodard <tnwoodard@gmail.com>
2022-02-24 16:47:38 +0000

    Artists' commissions page is complete

--- app/controllers/commissions_controller.rb
+++ app/controllers/commissions_controller.rb
@@ -1,5 +1,4 @@
 class CommissionsController < ApplicationController
-  # before_action :set_user
 
   def index
     @commissions = Commission.all
@@ -14,7 +13,8 @@ class CommissionsController < ApplicationController
   def create
     @commission = Commission.new(commission_params)
     @service = Service.find(commission_params[:service_id])
-    @commission.user = current_user
+    @user = @service.user
+    @commission.user = @user
     @commission.service = @service
     # @commission.art_price = params["commission"]["art_price"]
     @commission.art_price = @service.price
@@ -38,16 +38,6 @@ class CommissionsController < ApplicationController
     redirect_to commission_path(@commission)
   end
 
-  def my_bookings
-    @commissions = Commission.joins(:services).where.not(services: { id: nil })
-  end
-
-   # @services = Service.find(commission_params[:service_id])
-  #  @user = User.find(service_params[:user_id])
-   # @commissions.services = @commissions
-    #@commissions.count
-  #end
-
   private
 
   def commission_params
@@ -57,8 +47,4 @@ class CommissionsController < ApplicationController
   def service_params
     params.require(:service).permit(:name, :category, :price, :user_id)
   end
-  # def set_user
-  #   @user = User.find(params[:user_id])
-  # end
-
 end
