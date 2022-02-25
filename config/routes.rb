87218b2049a552418d01dc3a87a643d200cad7c2
Tempestt Woodard <tnwoodard@gmail.com>
2022-02-25 12:08:43 +0000

    save before pulling from master branch

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   delete '/services/:id', to: 'services#destroy', as: 'delete_service'
   get 'artists/:id', to: 'users#artists', as: 'artists'
   get 'artists/:id/edit', to: 'users#edit', as: 'edit_profile'
-  get '/digital_art', to: 'artworkss#digital_art', as: 'digital_art'
+  get '/digital_art', to: 'artworks#digital_art', as: 'digital_art'
   get '/paintings', to: 'artworks#paintings', as: 'paintings'
   get '/sculptures', to: 'artworks#sculptures', as: 'sculptures'
   get '/thanks', to: 'users#thanks', as: 'thanks'
